690154225ce25abea7cca6f91da8522df776dfd2
Adam Atyi <atyiadam@gmail.com>
2024-10-28 21:09:39 +0100

    Edit variables

--- prod/dns/variables.tf
+++ prod/dns/variables.tf
@@ -1,9 +1,16 @@
 variable "powerdns_api_key" {
-  type      = string
-  sensitive = true
+  description = "API key for authentication with PowerDNS server"
+  type        = string
+  sensitive   = true
+
+  validation {
+    condition     = length(var.powerdns_api_key) > 0
+    error_message = "PowerDNS API key cannot be empty"
+  }
 }
 
 variable "server_url" {
-  type = string
+  description = "Server URL for accessing the DNS server"
+  type        = string
 }
 
