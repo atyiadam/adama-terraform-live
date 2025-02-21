ccb1a08c4bd9deb9a6318082c8ab11fc39c21b6a
Adam Atyi <atyiadam@gmail.com>
2025-02-21 12:13:05 +0100

    Changes

--- prod/dns/main.tf
+++ prod/dns/main.tf
@@ -104,6 +104,46 @@ resource "powerdns_record" "jellyfin-a" {
   depends_on = [powerdns_zone.ns-home]
 }
 
+resource "powerdns_record" "prowlarr-a" {
+  zone    = "home.adamatyi.com."
+  name    = "prowlarr.home.adamatyi.com."
+  type    = "A"
+  ttl     = local.short_ttl
+  records = ["10.10.30.110"]
+
+  depends_on = [powerdns_zone.ns-home]
+}
+
+resource "powerdns_record" "radarr-a" {
+  zone    = "home.adamatyi.com."
+  name    = "radarr.home.adamatyi.com."
+  type    = "A"
+  ttl     = local.short_ttl
+  records = ["10.10.30.110"]
+
+  depends_on = [powerdns_zone.ns-home]
+}
+
+resource "powerdns_record" "sonarr-a" {
+  zone    = "home.adamatyi.com."
+  name    = "sonarr.home.adamatyi.com."
+  type    = "A"
+  ttl     = local.short_ttl
+  records = ["10.10.30.110"]
+
+  depends_on = [powerdns_zone.ns-home]
+}
+
+resource "powerdns_record" "qbittorrent-a" {
+  zone    = "home.adamatyi.com."
+  name    = "qbittorrent.home.adamatyi.com."
+  type    = "A"
+  ttl     = local.short_ttl
+  records = ["10.10.30.110"]
+
+  depends_on = [powerdns_zone.ns-home]
+}
+
 ## Zones
 resource "powerdns_zone" "prod" {
   name        = "prod.home.adamatyi.com."
